f196dacc6c42a5527f64a65df943b4b55115f89d
github username <etellezp@gmail.com>
2019-08-17 23:05:46 -0400

    Added axios for API requests and made a get request and render each pokemon to the DOM

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Pokedex = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => setPokemons(response.data.results))
+  }, [])
+
   return (
-    <h1>Hello from Pokedex</h1>
+    <>
+      <h1>Pokedex</h1>
+      {pokemons.map(pokemon => (
+        <p key={pokemon.name}>{pokemon.name}</p>
+      ))}
+    </>
   )
 }
 
-export default Pokedex 
+export default Pokedex
