fa4bce948d8493feb47ee332f6d8fa189a76b644
github username <etellezp@gmail.com>
2019-08-23 17:06:48 -0400

    Added the info that I wanted to the DOM from PokeApi to each specific pokemon in pokemon show page

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -1,9 +1,63 @@
 import React from 'react'
 
 function PokeCard(props) {
-  console.log("pokemonInfo", props.location.state.pokemonInfo.data)
+  const pokeData = props.location.state.pokemonInfo.data
+
   return (
-    <h1>Hello from PokeCard</h1>
+    <div>
+      {pokeData ?
+        <>
+          <img
+            src={pokeData.sprites.front_default} alt={pokeData.name}
+          />
+          <h1>{pokeData.name}</h1>
+          <h3>Height: {pokeData.height}</h3>
+          <h3>Weight: {pokeData.weight}</h3>
+          <h3>Stats:</h3>
+          {
+            pokeData.stats.map(stat => {
+              return (
+                <ul>
+                  <li>{stat.stat.name}: {stat.base_stat}</li>
+                </ul>
+              )
+            })
+          }
+          <h3>Abilities:</h3>
+          {
+            pokeData.abilities.map(ability => {
+              return (
+                <ul>
+                  <li>{ability.ability.name}</li>
+                </ul>
+              )
+            })
+          }
+          <h3>Type:</h3>
+          {
+            pokeData.types.map(type => {
+              return (
+                <ul>
+                  <li>{type.type.name}</li>
+                </ul>
+              )
+            })
+          }
+          <h3>Moves:</h3>
+          {
+            pokeData.moves.map(move => {
+              return(
+                <ul>
+                  <li>{move.move.name}</li>
+                </ul>
+              )
+            })
+          }
+        </>
+        :
+        "no data"
+      }
+    </div>
   )
 }
 
